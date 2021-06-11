68ea5064fae51ff90c830f6703a2531236b507cd
Yoomi Kim <yoomi@Yoomis-MacBook-Pro.local>
2021-06-11 01:14:42 -0700

    added nomics attribution

--- src/App.js
+++ src/App.js
@@ -4,13 +4,28 @@ import styled from '@emotion/styled';
 
 const Container = styled.div`
     background-color: white;
+    font-family: 'Lato', sans-serif;
+
+`;
+
+const NomicsAttribution = styled.span`
+    width: 90vw;
+    padding-top: 5px;
+    display: flex;
+    justify-content: flex-end;
+    text-decoration: underline;
+    color: black;
 `;
 
 const App = () => {
     return (
         <Container>
             <Market />
+            <NomicsAttribution>
+                <a href="https://nomics.com">Crypto Market Cap & Pricing Data Provided By Nomics</a>
+            </NomicsAttribution>
         </Container>
+
     )
 };
 
